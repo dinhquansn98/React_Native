8cbace85535f12795b9921dfb78f8484a264063a
unknown <dinhquansn@gmail.com>
2019-12-26 01:17:39 +0700

    Ghi chú Commit

--- SOURCE/RNbacsic/app/screens/UserScreen.js
+++ SOURCE/RNbacsic/app/screens/UserScreen.js
@@ -31,12 +31,31 @@ export default class UserScreen extends Component {
                 <View style={styles.user_fuction_block}>
                     {this._getFuncBlock("Đơn hàng", R.images.ic_order)}
                     {this._getFuncBlock("Cửa hàng", R.images.ic_store)}
-                    {this._getFuncBlock("Đơn hàng", require("../assets/images/ic_order.png"))}
-                    {this._getFuncBlock("Cửa hàng", require("../assets/images/ic_store.png"))}
-                    {this._getFuncBlock("Đơn hàng", require("../assets/images/ic_order.png"))}
-                    {this._getFuncBlock("Cửa hàng", require("../assets/images/ic_store.png"),true)}
-                    
+                    {this._getFuncBlock("Lịch sử giao dịch", require("../assets/images/ic_lsdd.png"))}
+                    {this._getFuncBlock("Trở thành đại lý", require("../assets/images/ic_trothanhdaily.png"))}
+                    {this._getFuncBlock("Thông tin bảo hành", require("../assets/images/ic_thongtinbaohang.png"))}
+                    {this._getFuncBlock("Thông tin về Daiichi", require("../assets/images/ic_ttdaiichi.png"))}
+                    {this._getFuncBlock("Đăng xuất", require("../assets/images/ic_dangxuat.png"), true)}
+
                 </View>
+                <View style={styles.container2}>
+                    <View style={styles.view_diem}>
+                        <Text style={styles.text_diem}>Điểm tích lũy:</Text>
+                        <Text style={styles.text_sodiem}>1200</Text>
+                    </View>
+                    <Image style={styles.img_line} source={require('../assets/images/ic_line1.png')} />
+                    <View style = {styles.view_bac}>
+                        <View style ={styles.line2} />
+                        <Text style={styles.text_bac}>Bạc</Text>
+                    </View>
+                    <Text style ={ styles.text_thanhvien}>Bạn đang là thành viên bạc của Daiichi</Text>
+                    <Text style= {styles.text_quyenloi}>Quyền Lợi:</Text>
+                    <View style={styles.gettext}>
+                        {this._getText("Chiết khấu 5% khi mua sản phẩm")}
+                        {this._getText("Có nhiểu ưu đãi và chương trình")}
+                    </View>
+                </View>
+
             </SafeAreaView>
         )
     }
@@ -56,6 +75,14 @@ export default class UserScreen extends Component {
                     />
                 </View>
                 {!isLast && <View style={styles.line}></View>}
+            </View>
+        )
+    }
+    _getText(title) {
+        return (
+            <View style={styles.gettext_view}>
+                <Image source={require('../assets/images/ic_dot.png')} />
+                <Text style={styles.gettext_text}>{title}</Text>
             </View>
         )
     }
@@ -144,6 +171,89 @@ const styles = StyleSheet.create({
         marginRight: 27,
         height: 2,
         backgroundColor: '#8B8B8B'
+    },
+    container2: {
+        backgroundColor: '#FFFFFF',
+        marginTop: 9
+    },
+    view_diem: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        backgroundColor: '#FFFFFF',
+        marginTop: 6,
+        marginLeft: 123,
+        marginRight: 122
+    },
+    text_diem: {
+        fontSize: 14,
+        color: '#EA4335'
+    },
+    text_sodiem: {
+        fontSize: 18,
+        color: '#EA4335',
+        marginLeft: 6
+    },
+    img_line: {
+        marginLeft: 22,
+        marginTop: 18,
+        marginRight: 22
+    },
+    gettext: {
+        marginLeft: 77,
+        marginRight: 77,
+        marginTop: 5,
+
+    },
+    gettext_view: {
+        marginTop: 5,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    gettext_img: {
+
+    },
+    gettext_text: {
+        marginLeft: 5
+    },
+    text_bac: {
+        textAlignVertical: "center",
+        textAlign: 'center',
+        backgroundColor: '#EA4335',
+        marginRight: 64,
+        marginLeft :64,
+        paddingHorizontal: 9,
+        paddingVertical: 3,
+        fontSize: 16,
+        borderRadius: 2,
+        overflow: 'hidden',
+        color: 'white',
+        fontFamily: 'Roboto-Regular',
+        
+    },
+    line2: {
+
+        backgroundColor: '#FF0000',
+        height:1,
+       
+      
+    },
+    view_bac :{
+        marginTop:18 ,
+        marginLeft :97 ,
+        marginRight:89
+    },
+    text_thanhvien :{
+        marginLeft:68 ,
+        marginRight:67,
+        marginTop :16,
+        fontSize :14
+    },
+    text_quyenloi:{
+        marginRight:156,
+        marginLeft:157,
+        marginTop:5,
+        fontSize :14,
+        fontFamily :'Medium'
     }
 })
 
